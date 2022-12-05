a75e089a422827f8a30bff6c0b0c8645a51c4d97
Joshua Niemelä <josh@jniemela.dk>
2022-12-05 16:51:55 +0100

    Explained regex

--- day5/src/day5.jl
+++ day5/src/day5.jl
@@ -2,8 +2,14 @@ data = eachline("input") |> collect
 
 blank = findfirst(isempty, data)
 
+# The regex string matche to the following:
+# Everything followed by move, from, to (with spaces in between)
 moves = data[blank+1:end] .|> x -> parse.(Int, match(r"move (\d+) from (\d+) to (\d+)", x).captures)
 
+#= 
+The regex string matche to the following:
+Finds the capitalised letters between square brackets OR 4 consecutive whitespaces
+since the whitespace is not captured, Julia will return 'nothing' =#
 crateArray = map(eachmatch.(r"\[([A-Z])\]|\ {4}", data[begin:blank-2])) do row
     row .|> x -> x.captures[1] === nothing ? nothing : x.captures[1][1]
 end |> x -> hcat(x...)
