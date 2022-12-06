12de6259867f618ffe67a018f6deb847e789c833
Joshua Niemelä <josh@jniemela.dk>
2022-12-06 10:44:00 +0100

    Changed function

--- day06/src/Main.hs
+++ day06/src/Main.hs
@@ -2,15 +2,14 @@ module Main where
 import Data.List
 
 -- Check if a string contains n disjunct characters in a row and return the index of the last character
-findN :: String -> Int -> Int -> Int
-findN (a:b:xs) n i
-  | length (nub (take n (a:b:rest))) == n = i+n
-  | otherwise = findN (b:rest) n (i+1)
+findN :: String -> Int -> Int
+findN (a:b:rest) n = 
+  if length (nub (take n (a:b:rest))) /= n then findN (b:rest) n + 1 else n
 
 main = do
   s <- readFile "input"
-  let part1 = findN s 4 0
-  let part2 = findN s 14 0
+  let part1 = findN s 4
+  let part2 = findN s 14
   
   print $ "Part 1: " ++ show part1
   print $ "Part 2: " ++ show part2
