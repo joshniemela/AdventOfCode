bede095ce21c39edc472428ee1578335c161c50a
Joshua Niemelä <josh@jniemela.dk>
2022-12-06 01:58:42 +0100

    moved stuff

--- grabinput.sh
+++ grabinput.sh
@@ -3,4 +3,4 @@
 day=$1
 
 set -e
-curl -f --cookie "session=`cat .session`" https://adventofcode.com/2021/day/$day/input > day$(printf "%02d\n" $day)/input.txt
+curl -f --cookie "session=`cat .session`" https://adventofcode.com/2022/day/$day/input > day$(printf "%02d\n" $day)/input
